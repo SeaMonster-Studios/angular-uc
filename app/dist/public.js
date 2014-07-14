bb2bb1c3021a8c7ce81368a01072cd1d189fe362
Nick Harberg <nick@seamonsterstudios.com>
2014-07-14 10:03:36 -0700

    still working on cartController.js

--- app/dist/public.js
+++ app/dist/public.js
@@ -23734,25 +23734,25 @@ module.exports = function(app) {
             var cartId = myCart.cartId;
             var id = "SEAM-ITEM-001";
             console.log("the cartId is: " + cartId);
+
             var data = JSON.stringify({merchantId:merchantId, cartId: cartId});
             if(cartId) {
                 $http({
-                    url: itemUrl + encodeURIComponent(id) + "&_mid=" + merchantId + "/" + cartId,
-                    method: "PUT",
-                    //data: data,
+                    url: itemUrl + encodeURIComponent(id) + "&_mid=" + merchantId,
+                    method: "POST",
+                    data: data,
                     dataType: "json"
                 })
                 .success(function(data, status, headers, config) {
                     window.myItem = data;
-                    console.log("myItem is: " + data);
-                    $scope.displayItem = data;
+                    console.dir(myItem);
+                    $scope.displayItem = myItem;
                 })
                 .error(function(data, status, headers, config) {
                     console.log("there was an error: " + data);
                 }); // end $http.post
             }
         }
-
     });// end app.controller("CartController")
 };// end module.exports
 
