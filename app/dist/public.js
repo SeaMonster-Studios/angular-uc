0802db6e9fdd0bb90b69b4ea5bd8633c72e1f2bc
Nick Harberg <nick@seamonsterstudios.com>
2014-07-15 11:06:48 -0700

    add item to cart is working now.

--- app/dist/public.js
+++ app/dist/public.js
@@ -23758,15 +23758,18 @@ module.exports = function(app) {
 
         //}// end $scope.createCart()
         $scope.createCart = function() {
+            console.log("createCart called");
             CreateCart.create();
         };
 
-        $scope.addItem = function() {
+        $scope.loadItem = function() {
             var cartId = myCart.cartId;
             var id = "SEAM-ITEM-001";
             console.log("the cartId is: " + cartId);
 
             var data = JSON.stringify({merchantId:merchantId, cartId: cartId});
+            console.log("the data is: " + data);
+
             if(cartId) {
                 $http({
                     url: itemUrl + encodeURIComponent(id) + "&_mid=" + merchantId,
@@ -23776,14 +23779,44 @@ module.exports = function(app) {
                 })
                 .success(function(data, status, headers, config) {
                     window.myItem = data;
-                    console.dir(myItem);
+                    //console.dir(myItem);
                     //$scope.cartDisplay = myCart;
                     $scope.displayItem = myItem;
+                    return myItem;
                 })
                 .error(function(data, status, headers, config) {
                     console.log("there was an error: " + data);
                 }); // end $http.post
             }
+        }// end $scope.loadItem
+
+        $scope.addItem = function() {
+            console.log("inside of addItem()");
+
+            //var id = "SEAM-ITEM-001";
+            var itemId = window.myItem.itemId;
+            console.dir(itemId);
+            if(itemId) {
+                if(!myCart.items) {
+                    myCart['items'] = [];
+                }
+                myCart.items.push({itemId: itemId, quantity: 1});
+                var jCart = JSON.stringify(myCart);
+                $http({
+                    url: cartUrl,
+                    method: "POST",
+                    data: jCart,
+                    dataType: "json"
+                })
+                .success(function(data, status, headers, config) {
+                    $scope.cartDisplay = data;
+                    console.log("addItem success");
+                })
+                .error(function(data, status, headers, config) {
+                    console.log("there was an error with addItem(): " + data);
+                }); // end $http.post
+            }
+
         }
     });// end app.controller("CartController")
 };// end module.exports
