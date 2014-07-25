f21449a1cc1e47d2bcfe8dee1b011b1303fa5983
Nick Harberg <nick@seamonsterstudios.com>
2014-07-25 13:55:23 -0700

    saving cc info to the cart

--- app/dist/public.js
+++ app/dist/public.js
@@ -23923,9 +23923,50 @@ var itemUrl    = baseUrl.itemUrl;
 var merchantId = baseUrl.merchantId;
 var checkoutUrl = baseUrl.checkoutUrl;
 
-modulel.exports = function(app) {
+module.exports = function(app) {
     app.controller("CheckoutPaymentController", function($scope, $http, $location, $q, ipCookie, CreateCart, LoadCart) {
+        $scope.message = "I am in the CheckoutPaymentController";
+        $scope.ccInfo = {};
+        var cart = {};
+        var creditCard = {
+            type   : ['AMEX', 'MasterCard', 'Visa'],
+            month  : ["January","February","March","April","May","June","July","August","September","October","November","December"],
+            year   : [2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026,2027],
+            number : 0,
+            csv : 0
+        }
+        $scope.creditCard = creditCard;
 
+        $scope.saveCC = function(cc) {
+            $scope.ccInfo = angular.copy(cc);
+            console.log($scope.ccInfo);
+
+            myCart.creditCardExpirationMonth = $scope.ccInfo.month;
+            myCart.creditCardExpirationYear = $scope.ccInfo.year;
+            myCart.creditCardType = $scope.ccInfo.type;
+            myCart.creditCardVerificationNumber = $scope.ccInfo.csv;
+            myCart.creditCardNumber = $scope.ccInfo.number;
+
+            var jCart = JSON.stringify(myCart);
+            return $http({
+                url: cartUrl,
+                method: "POST",
+                data: jCart,
+                params: {_mid: merchantId, _cid: ipCookie("UltraCartShoppingCartID")},
+                dataType: "json",
+                cache: false
+            })
+            .success(function(cart, status, headers, config) {
+                console.log("inside success for saving CC info");
+                window.myCart = cart;
+                return cart;
+            })
+            .error(function(cart, status, headers, config) {
+                console.log("there was an error with CC: " + cart);
+            }); // end $http(post)
+            return cart;
+        }
+        return cart;
     });// end app.controller("CheckoutPaymentController")
 }; // end modulel.exports
 },{"../../../../api/db":1}],13:[function(require,module,exports){
